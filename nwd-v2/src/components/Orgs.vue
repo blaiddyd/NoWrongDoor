ad4e27d0ceeb016dd79d59e4893b732073d36a40
ekinbukulmez <22412569@student.uwa.edu.au>
2019-01-15 21:45:11 +0800

    initial commit of Vue version

---
+++ nwd-v2/src/components/Orgs.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-content>
+        <v-toolbar>
+            <v-toolbar-title>
+                No Wrong Door
+            </v-toolbar-title>
+        </v-toolbar>
+    </v-content>
+</template>
---
+++ nwd-v2/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Orgs></Orgs>
+  </div>
+</template>
+
+<script>
+  import Orgs from '@/components/Orgs.vue';
+
+  export default {
+    component: Orgs,
+  };
+</script>
